f4c7b0c961f6f7707b1ab8e82c6ffec40251d697
parthak314 <parthakfree@gmail.com>
2024-10-17 21:52:25 +0100

    commit

--- counter_tb.cpp
+++ counter_tb.cpp
@@ -18,7 +18,7 @@ int main(int argc, char **argv, char **env) {
 
     // init Vbuddy
     if (vbdOpen()!=1) return(-1);
-    vbdHeader("Lab 1: Counter");
+    vbdHeader("Partha Khanna");
 
     // initialize simulation inputs
     top->clk = 1;
@@ -34,16 +34,16 @@ int main(int argc, char **argv, char **env) {
             top->clk = !top->clk;
             top->eval ();
         }
-
-        vbdHex(4, (int(top->count) >> 16) & 0xF);
-        vbdHex(3, (int(top->count) >> 8) & 0xF);
-        vbdHex(2, (int(top->count) >> 4) & 0xF);
-        vbdHex(1, int(top->count) & 0xF);
+    
+        // vbdHex(4, (int(top->count) >> 16) & 0xF);
+        // vbdHex(3, (int(top->count) >> 8) & 0xF);
+        // vbdHex(2, (int(top->count) >> 4) & 0xF);
+        // vbdHex(1, int(top->count) & 0xF);
         vbdCycle(i+1);
         // end of Vbuddy output section
 
         top->rst = (i < 2) | (i == 15);
-        top->en = (i > 4);
+        top->en = vbdFlag();
         if (Verilated::gotFinish()) exit(0);
     }
     vbdClose();
