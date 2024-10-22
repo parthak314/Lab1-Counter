74ba726ccad25be4c120d074dd841b4bc67c7c05
parthak314 <parthakfree@gmail.com>
2024-10-22 22:14:07 +0100

    Task 1

---
+++ task1/counter.sv
@@ -0,0 +1,18 @@
+module counter #(
+    parameter WIDTH = 8
+)(
+    // interface signals
+    input logic          clk,    // clock
+    input logic          rst,    // reset
+    input logic          en,     // counter enable
+    output logic [WIDTH-1:0] count  // count output
+);
+
+// Internal register to halt counting
+always_ff @(posedge clk or posedge rst) begin
+    if (count )
+    if (rst) count <= {WIDTH{1'b0}};
+    else     count <= count + {{WIDTH-1{1'b0}}, en};
+end
+
+endmodule
